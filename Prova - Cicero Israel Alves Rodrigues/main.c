9a94cad2347e406361a919dc7f5ac3c5a07bd16a
DESKTOP-G3FO92L\UFCA <A@A.COM>
2022-10-06 12:01:53 -0300

    adiciono a quarta questão com vetores

--- Prova - Cicero Israel Alves Rodrigues/main.c
+++ Prova - Cicero Israel Alves Rodrigues/main.c
@@ -160,3 +160,46 @@ int main()
     return 0;
 }
 */
+
+int main()
+{
+    setlocale(LC_ALL, "Portuguese");
+    int n ,i, aprovado = 0, reprovado = 0, avf = 0;
+    float notas[50], maior = 0, menor = 0, media = 0;
+
+    printf("Digite a quantidade de alunos de 1 a 50: ");
+    scanf("%d", &n);
+    while(n > 50 || n < 1){
+        printf("Por favor! Digite a quantidade de alunos de 1 a 50: ");
+        scanf("%d", &n);
+    }
+
+    for(i = 0; i < n; i++){
+        printf("Nota do aluno %d: ", i+1);
+        scanf("%f", &notas[i]);
+
+        if(i==0) maior = notas[i], menor = notas[i];
+        if(notas[i] > maior) maior = notas[i];
+        if(notas[i] < menor) menor = notas[i];
+
+
+        if(notas[i] >= 7) aprovado++;
+        else if(notas[i] < 3) reprovado++;
+        else if(notas[i] >= 3&& notas[i] < 7) avf++;
+
+        media += notas[i];
+    }
+
+    media = media / n;
+
+    printf("\n\nMaior nota: %f ", maior);
+    printf("\n\nMenor nota: %f ", menor);
+    printf("\n\nQuantidade de aprovados: %d ", aprovado);
+    printf("\n\nQuantidade de reprovados: %d ", reprovado);
+    printf("\n\nQuantidade que vão para AVF: %d ", avf);
+    printf("\n\nMédia das notas: %f ", media);
+
+
+    return 0;
+}
+
